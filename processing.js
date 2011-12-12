a4a9c26269070b74b3855a803bf6b9526b1f0a8c
Greg Krilov <gkrilov@gmail.com>
2011-12-12 13:44:23 -0500

    initial final fix changes

--- processing.js
+++ processing.js
@@ -2320,11 +2320,14 @@
     // Objects for shared, 2D and 3D contexts
     var DrawingShared = function() {};
     var Drawing2D = function() {};
+    var Drawing3D = function() {};
     var DrawingPre = function() {};
 
     // Setup the prototype chain
     Drawing2D.prototype = new DrawingShared();
     Drawing2D.prototype.constructor = Drawing2D;
+    Drawing3D.prototype = new DrawingShared();
+    Drawing3D.prototype.constructor = Drawing3D;
     DrawingPre.prototype = new DrawingShared();
     DrawingPre.prototype.constructor = DrawingPre;
 
@@ -4117,41 +4120,7 @@
      * @see shapeMode()
      */
     p.shape = function(shape, x, y, width, height) {
-      if (arguments.length >= 1 && arguments[0] !== null) {
-        if (shape.isVisible()) {
-          p.pushMatrix();
-          if (curShapeMode === PConstants.CENTER) {
-            if (arguments.length === 5) {
-              p.translate(x - width/2, y - height/2);
-              p.scale(width / shape.getWidth(), height / shape.getHeight());
-            } else if (arguments.length === 3) {
-              p.translate(x - shape.getWidth()/2, - shape.getHeight()/2);
-            } else {
-              p.translate(-shape.getWidth()/2, -shape.getHeight()/2);
-            }
-          } else if (curShapeMode === PConstants.CORNER) {
-            if (arguments.length === 5) {
-              p.translate(x, y);
-              p.scale(width / shape.getWidth(), height / shape.getHeight());
-            } else if (arguments.length === 3) {
-              p.translate(x, y);
-            }
-          } else if (curShapeMode === PConstants.CORNERS) {
-            if (arguments.length === 5) {
-              width  -= x;
-              height -= y;
-              p.translate(x, y);
-              p.scale(width / shape.getWidth(), height / shape.getHeight());
-            } else if (arguments.length === 3) {
-              p.translate(x, y);
-            }
-          }
-          shape.draw();
-          if ((arguments.length === 1 && curShapeMode === PConstants.CENTER ) || arguments.length > 1) {
-            p.popMatrix();
-          }
-        }
-      }
+      throw("SVG shapes are not supported in IE8");
     };
 
     /**
